47f9626251c16d00c65128d73a1f385f23015ae1
Polina <paulina.northy@gmail.com>
2023-08-02 18:04:18 +0100

    init

---
+++ src/main/java/uk/bovykina/calculator/service/ResultWriterService.java
@@ -0,0 +1,7 @@
+package uk.bovykina.calculator.service;
+
+public class ResultWriterService {
+    public static void printResult(float n1, float n2, float result, String action) {
+        System.out.println(n1+" "+action+" "+n2+" = "+result);
+    }
+}
---
+++ src/main/java/uk/bovykina/calculator/util/Calculator.java
@@ -0,0 +1,16 @@
+package uk.bovykina.calculator.util;
+
+public class Calculator {
+    public static float add(float n1, float n2) {
+        float sum = n1+n2;
+        return sum;
+    }
+    public static float substract(float n1, float n2) {
+        float sub = n1-n2;
+        return sub;
+    }
+    public static float multiply(float n1, float n2) {
+        float mult = n1*n2;
+        return mult;
+    }
+}
